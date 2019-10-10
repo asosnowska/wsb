f644ff86d42f98b1db9d8b93fb86c1d6b3aa6450
asosnowska <56115971+asosnowska@users.noreply.github.com>
2019-10-10 14:40:52 +0200

    Add files via upload

---
+++ podstawy_programowania/zajecia2c.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//pętla for wyswietl liczby z przedzialu
+
+            for (int i = 0; i<=10;i++)
+            {
+                Console.Write("{0} ", i);
+
+            }
+            //wyświetl liczby parzyste z przedziału <5;20> w porządku malejącym
+
+            for (int i = 20; i>=5; i--)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+                /* wyświetl na ekranie:
+                 * *
+                 * **
+                 * ***
+                 * ****
+                 * *****
+                 * wysokość choinki użytkownik podaje z klawiatury
+                 */
+
+                //pętla - w którym wierszu wyświetlamy 
+                //for (int i=1; i<=ile; i++)
+                //pętla - ile gwiazdek wyświetlamy
+                for (int j = 1; i <= 5; i++)
+                {
+                    Console.Write("*", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
