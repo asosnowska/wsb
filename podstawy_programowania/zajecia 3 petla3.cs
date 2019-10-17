1580824619540edda626e763834844cd421f05c7
asosnowska <56115971+asosnowska@users.noreply.github.com>
2019-10-17 14:48:18 +0200

    Add files via upload

---
+++ podstawy_programowania/zajecia 3 petla3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("tabliczka mnozenia");
+            int n = 10, row, col;
+            row = 1;
+            do
+            {
+                col = 1;
+                do
+                {
+                    //Console.Write(row * col + "\t");
+                    Console.Write("{0}\t", row * col);
+                    col++;
+
+                } while (col <= n);
+                row++;
+                Console.WriteLine();
+            } while (row <= n);*/
+
+            /*
+             * Napisz program, który wyświetli duże litery alfabetu
+             * od A do Z i od Z do A z wykorzystaniem pętli for
+             * np. A, B, C, ..., Z.
+             * np. Z, Y, X, ..., A.
+             * */
+
+            //continue
+            int j = 1;
+            for (; j <= 10; j++)
+            {
+                if (j == 2 || j == 5 || j == 7)
+                    continue;
+                else
+                    Console.Write("{0}", j);
+            }
+            Console.WriteLine();
+                    
+
+           
+        }
+    }
+}
---
+++ podstawy_programowania/zajecia 3 petla4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Napisz program, który w 10-elementowej tablicy jednowymiarowej o nazwie tab umieszcza liczby całkowite
+             * z przedziału od 1 do 10
+             */
+            int n = 10, i;
+            //deklaracja tablicy o nazwie tab typu całkowitego o rozmiarze n = 10
+            int[] tab = new int[n];
+            for (i=0; i< tab.Length; i++)
+            {
+                Console.Write("{0}", tab[i]); ;
+            }
+            /*Napisz program, który w 5-elementowej tablicy jednowymiarowej o nazwie colors
+             * umieszcza nazwy kolorów podane z klawiatury przez użytkownika
+             * wyświetl kolory w formacie(while):
+             * kolor 1: red
+             * kolor 2: blue
+             * ...
+             * kolor 5: yellow
+             */
+            string[] colors = new string[5];
+            string color;
+            for (i=0; i < colors.Length; i++)
+            {
+                Console.Write("Podaj kolor:");
+                color = Console.ReadLine();
+                colors[i] = color; 
+                   
+            }
+        }
+    }
+}
