d7f87514d079e5f0f986b98de33b683b5aaf4fee
asosnowska <56115971+asosnowska@users.noreply.github.com>
2019-10-17 14:48:00 +0200

    Add files via upload

---
+++ podstawy_programowania/zajecia 3 petla2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            while (x<=15)
+            {
+                y = 4 * x;
+                Console.WriteLine("x=" + x + "y=" + y);
+                x++;
+            }
+        }
+    }
+}
